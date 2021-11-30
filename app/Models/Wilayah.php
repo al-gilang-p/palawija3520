4aba65c91fa1c3765025558e8da11930402e910c
Alwin Gilang Permana <al.gilang.p@bps.go.id>
2021-11-30 21:58:30 +0700

    add migration, controller, and model for wilayah

---
+++ app/Models/Wilayah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Wilayah extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['sr', 'kd_kec', 'nm_kec', 'kd_desa', 'nm_desa', 'nbs', 'nks', 'id_segmen', 'nm_lokasi', 'ar', 'subsegmen', 'bln_panen', 'kd_pcl', 'nm_pcl', 'kd_pml', 'nm_pml'];
+}
---
+++ database/migrations/2021_11_30_134427_create_wilayahs_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWilayahsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wilayahs', function (Blueprint $table) {
+            $table->id();
+            $table->enum('sr', ['1', '2', '3']);
+            $table->char('kd_kec', 3);
+            $table->string('nm_kec', 100);
+            $table->char('kd_desa', 3);
+            $table->string('nm_desa', 100);
+            $table->char('nbs', 4);
+            $table->char('nks', 8);
+            $table->char('id_segmen', 9);
+            $table->string('nm_lokasi', 100);
+            $table->float('ar');
+            $table->char('subsegmen', 2);
+            $table->char('bln_panen', 2);
+            $table->char('kd_pcl', 3);
+            $table->string('nm_pcl', 100);
+            $table->char('kd_pml', 3);
+            $table->string('nm_pml', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wilayahs');
+    }
+}
