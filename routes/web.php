7e03a92dcdf7090b94889adcb36ce2d1e5981412
Alwin Gilang Permana <al.gilang.p@bps.go.id>
2021-12-21 22:02:44 +0700

    Add login page

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
 Route::get('/', function () {
     return view('admin.pages.dashboard');
 })->name('admin.dashboard');
